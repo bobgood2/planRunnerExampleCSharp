6af6505b1f9da77eaf6a668156fac0689bd164f2
Bob Goodwin <bobgood@microsoft.com>
2024-06-12 19:35:18 -0700

    fix comment

--- Plan_Server/lib.py
+++ Plan_Server/lib.py
@@ -1,6 +1,11 @@
 
 # example on how to set up a library of additional functions that can be called from within
 # executed code.  for example otherFunc() in global namespace will ending up returning 2.
+#
+# the setup method is called each time code is executed, so all fields stored in the class have the scope
+# of the current execution, and can be shared across functions in the class, but will not be global to the
+# original executed code.
+#
 # the reason the the orchestrator is provided is so that you can dynamically grow the workflow Dag
 # with these functions if you want non-blocking calls  (don't use async here)
 # see orchestrator.py
